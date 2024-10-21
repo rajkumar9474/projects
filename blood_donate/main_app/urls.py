fd2a05d7ed01bd963f4c19fe9a98b70d03088b69
rajkumar9474 <raj9434153@gmail.com>
2024-10-21 21:04:13 +0530

    responsive fixed

---
+++ blood_donate/main_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from main_app import views
+
+urlpatterns = [
+    path('',views.home),
+    
+    path('index/',views.index,name='index'),
+    path('login/',views.login,name='login'),
+]
